2b5c6023f40f20563777f031a41978a3b8d71ec3
chadbutz <chadfeigenbutz@gmail.com>
2017-09-30 12:20:17 -0400

    None

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,2 @@
-create table conversation (
-    id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
-    email VARCHAR(50),
-    PRIMARY KEY (id)
-);
-
 INSERT INTO conversation (name, email) VALUES ('Hello', 'chad@chad.com');
+INSERT INTO conversation (name, email) VALUES ('Yo', 'butz@chad.com');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+drop table conversation;
+
+create table conversation (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    PRIMARY KEY (id)
+);
