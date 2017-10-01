290bcf64ea0e4529532f5a99594f628a869e478a
chadbutz <chadfeigenbutz@gmail.com>
2017-09-30 20:24:53 -0400

    None

--- src/main/java/com/chadbutz/pai/main/Application.java
+++ src/main/java/com/chadbutz/pai/main/Application.java
@@ -2,21 +2,9 @@ package com.chadbutz.pai.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-//		(
-//    scanBasePackages = {
-//        "com.chadbutz.pai.model","com.chadbutz.pai.main"
-//    }
-//)
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
